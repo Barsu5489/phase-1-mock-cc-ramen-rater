9522a9efbe439dc10ef949efe84ad3e23286fd1c
barsu5489 <ebarsulai@gmail.com>
2022-12-27 17:43:09 +0300

    info about a specific ramen on click

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 
 const ramenMenu = document.getElementById('ramen-menu')
+const reamenDetail = document.getElementById('ramen-detail') 
 
 function renderImg(img){
 for(let i = 0; i<img.length; i++){
@@ -8,6 +9,14 @@ for(let i = 0; i<img.length; i++){
     console.log('hh')
     imgContent.classList.add('detail-image')
     ramenMenu.appendChild(imgContent)
+    imgContent.addEventListener('click',()=>{
+        reamenDetail.innerHTML = `   <img class="detail-image" src="${img[i].image}" alt="Insert Name Here" />
+        <h2 class="name">${img[i].name}</h2>
+        <h3 class="restaurant">${img[i].restaurant}</h3>`
+
+        console.log(img[i].name)
+    })
+
 }
    
 }
@@ -15,7 +24,6 @@ function fetchContent(){
     fetch('http://localhost:3000/ramens')
     .then(res=>res.json())
     .then(data=>{
-        console.log(data[0].image)
         renderImg(data)
     })
 }
