847a122350bedf41f93db7426ceb549d755a84e3
barsu5489 <ebarsulai@gmail.com>
2022-12-27 18:10:40 +0300

    added comment and rating

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 
 const ramenMenu = document.getElementById('ramen-menu')
 const reamenDetail = document.getElementById('ramen-detail') 
+const rating = document.getElementById('rating-display')
+const comment = document.getElementById('comment-display')
 
 function renderImg(img){
 for(let i = 0; i<img.length; i++){
@@ -13,13 +15,28 @@ for(let i = 0; i<img.length; i++){
         reamenDetail.innerHTML = `   <img class="detail-image" src="${img[i].image}" alt="Insert Name Here" />
         <h2 class="name">${img[i].name}</h2>
         <h3 class="restaurant">${img[i].restaurant}</h3>`
-
+        rating.textContent = img[i].rating
+        comment.textContent = img[i].comment
         console.log(img[i].name)
     })
 
+}  
 }
-   
+
+function formdata(){
+    const form = document.getElementById('new-ramen')
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault()
+        let name = e.target.name.value
+        let restaurant = e.target.restaurant.value
+        let img = e.target.image.value
+        let rating = e.target.rating.value
+        let comment = e.target.newComment.value
+        console.log(name, restaurant,img,rating,comment)
+        
+    })
 }
+formdata()
 function fetchContent(){
     fetch('http://localhost:3000/ramens')
     .then(res=>res.json())
