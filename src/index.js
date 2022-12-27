c646a6ca5cca8ea6ca037b1ba8cdd495c418e589
barsu5489 <ebarsulai@gmail.com>
2022-12-27 17:01:32 +0300

    rendered food images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-// write your code here
+
+const ramenMenu = document.getElementById('ramen-menu')
+
+function renderImg(img){
+for(let i = 0; i<img.length; i++){
+    const imgContent = document.createElement('img')
+    imgContent.src = img[i].image 
+    console.log('hh')
+    imgContent.classList.add('detail-image')
+    ramenMenu.appendChild(imgContent)
+}
+   
+}
+function fetchContent(){
+    fetch('http://localhost:3000/ramens')
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data[0].image)
+        renderImg(data)
+    })
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+fetchContent()
+})
