27355ee0178fc72dd8010697bd771b67c7d10ead
barsu5489 <ebarsulai@gmail.com>
2022-12-27 20:04:34 +0300

    able to create new ramen with details and add to the list

--- src/index.js
+++ src/index.js
@@ -27,11 +27,29 @@ function formdata(){
     const form = document.getElementById('new-ramen')
     form.addEventListener('submit',(e)=>{
         e.preventDefault()
+        const ratinng = document.getElementById('rating-display')
+        const commentt = document.getElementById('comment-display')
         let name = e.target.name.value
         let restaurant = e.target.restaurant.value
         let img = e.target.image.value
         let rating = e.target.rating.value
         let comment = e.target.newComment.value
+        const imag = document.createElement('img')
+        imag.src = img
+        imag.classList.add('detail-image')
+        ramenMenu.appendChild(imag)
+       
+        imag.addEventListener('click',()=>{
+
+            reamenDetail.innerHTML = `<img class="detail-image" src="${img}" alt="Insert Name Here" />
+            <h2 class="name">${name}</h2>
+            <h3 class="restaurant">${restaurant}</h3>`
+            // rating.textContent = ''
+            // comment.textContent = ''
+            ratinng.textContent = rating
+            commentt.textContent = comment
+        })
+        console.log(imag)
         console.log(name, restaurant,img,rating,comment)
         
     })
